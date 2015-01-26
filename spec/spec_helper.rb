22f435a52d53159161fe2289ccb8ac7aa0139f88
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2015-01-26 18:02:20 +1100

    added FG methods + SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+require 'simplecov'
+SimpleCov.start 'rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -29,7 +32,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
   
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
